6bbdcfbd7e8b5c7b890f11a9d902578fa9f74520
Roger Lee <rmlee0003@gmail.com>
2019-08-07 19:58:35 -0400

    created get users endpoint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+
+
+// Get users
+server.get('/api/users',(req,res) => {
+ db.find()
+ .then(hubs => {
+  res.json(hubs)
+ })
+ .catch(err => {
+  res.status(500).json({
+   error: "The users information could not be retrieved."
+  })
+ })
+})  
+
+
+// run server
+server.listen(4000, () => {
+ console.log('server is running')
+});
