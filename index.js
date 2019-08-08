87f50d26557805ee23d8b90d70119c2e48a6077d
Roger Lee <rmlee0003@gmail.com>
2019-08-07 20:40:32 -0400

    created get user by id endpoint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // implement your API here
 const express = require('express')
 const db = require('./data/db')
+const server = express()
 
-
+server.use(express.json())
 
 // Get users
 server.get('/api/users',(req,res) => {
@@ -17,6 +18,49 @@ server.get('/api/users',(req,res) => {
  })
 })  
 
+//post newUser
+server.post('/api/users', (req,res) => {
+ const newUser = req.body
+ if(newUser.name && newUser.bio){
+   db.insert(newUser)
+   
+   .then(id => {
+    res.status(201).json(id)   
+ })
+   .catch(err => {
+    res.status(404).json({
+     error:"There was an error while saving the user to the database"
+    })
+   })
+}
+ else{ 
+  res.status(400).json({
+   errorMessage: "Please provide name and bio for the user"
+  })
+ }
+})
+
+//GET user/id
+server.get('/api/users/:id', (req,res) => {
+ const {id} = req.params
+ db.findById(id)
+ .then(hub => {
+  console.log(id)
+  if(hub){
+   res.json(hub)
+  }
+  else {
+   res.status(404).json({
+    message: "The user with the specified ID does not exist"
+   })
+  }
+ })
+ .catch(err => {
+  res.status(500).json({
+   message: "The user information could not be retrieved"
+  })
+ })
+})
 
 // run server
 server.listen(4000, () => {
